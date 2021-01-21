ddc7ae0b2efeb6633bee756c1208aae7d0647bab
20odeebh <20odeebh@gmail.com>
2021-01-21 23:25:15 +0900

    210119

--- eunbin/크레인 인형뽑기 게임.cpp
+++ eunbin/크레인 인형뽑기 게임.cpp
@@ -6,6 +6,7 @@ stack<int> stk;
 int rt;
 
 int solution(vector<vector<int>> board, vector<int> moves) {
+	
 	for (int i = 0; i < moves.size(); i++) {
 		for (int j = 0; j < board.size(); j++) {
 			int x = moves[i] - 1;
