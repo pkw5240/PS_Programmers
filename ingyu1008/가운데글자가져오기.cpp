7552360e15824d83b39ca3b9077e0d2e7fec4caf
byeon <bso11246@gmail.com>
2021-01-25 19:10:49 +0900

    Add diskcontroller

--- ingyu1008/가운데글자가져오기.cpp
+++
@@ -1,8 +0,0 @@
-#include <string>
-#include <vector>
-
-std::string solution(std::string s)
-{
-    std::string answer = s.substr((s.size() - 1) / 2, (s.size() & 1) ? 1 : 2);
-    return answer;
-}
--- ingyu1008/같은숫자는싫어.cpp
+++
@@ -1,8 +0,0 @@
-#include <vector>
-#include <algorithm>
-
-std::vector<int> solution(std::vector<int> arr)
-{
-    arr.erase(unique(arr.begin(), arr.end()), arr.end());
-    return arr;
-}
