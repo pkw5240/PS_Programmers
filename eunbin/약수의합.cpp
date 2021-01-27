5f323f0bd5ec7d636d67bcf7af2dc49631e384ff
20odeebh <31841180+20odeebh@users.noreply.github.com>
2021-01-27 22:08:25 +0900

    Update 약수의합.cpp

--- eunbin/약수의합.cpp
+++ eunbin/약수의합.cpp
@@ -5,14 +5,8 @@ using namespace std;
 
 int solution(int n) {
     int answer = 0;
-    for(int i=1;i<=n;i++){
-        
-        if( n%i == 0) answer += i;
-        
-        
-    }
-    
-    
-    
+    for(int i=1;i<=n;i++){        
+        if( n%i == 0) answer += i;                
+    }    
     return answer;
 }
