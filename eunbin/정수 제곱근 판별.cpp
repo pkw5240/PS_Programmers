5b7a8b61bf880b1be88b2cfde6afe482efe6dde3
20odeebh <31841180+20odeebh@users.noreply.github.com>
2021-01-27 22:53:21 +0900

    Update 정수 제곱근 판별.cpp

--- eunbin/정수 제곱근 판별.cpp
+++ eunbin/정수 제곱근 판별.cpp
@@ -1,21 +1,9 @@
-#include <string>
-#include <vector>
-#include <cmath>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-//1. 제곱근 구함 2. 자연수 인지 확인 3. 맞으면 제곱 아니면 -1
-
-    long long solution(long long n) {
+long long solution(long long n) {
     long long answer = 0;
-    
-    long long cand = sqrt(n);
-    if(cand * cand == n) {
-        answer = (cand+1)  * (cand+1);
-    } else {
-        answer = -1;
-    }
-    
+    answer=(sqrt(n) == (int)sqrt(n))? pow(sqrt(n)+1,2):-1;
     return answer;
-
-
 }
