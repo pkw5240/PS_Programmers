dea86dc291c370c8c08a9262bc8eee8870ce3fd6
20odeebh <31841180+20odeebh@users.noreply.github.com>
2021-01-27 21:54:33 +0900

    Update 수바수박수박수박수박수.cpp

--- eunbin/수바수박수박수박수박수.cpp
+++ eunbin/수바수박수박수박수박수.cpp
@@ -5,11 +5,8 @@ using namespace std;
 
 string solution(int n) {
     string answer = "";
-    for(int i = 0; i < n; i++){
-        
-        if((i % 2) == 0) answer += "수";
-        else answer += "박";
+    for(int i=0;i<n;i++){
+        answer += i%2? "박":"수";
     }
     return answer;
-    
 }
