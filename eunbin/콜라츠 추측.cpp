35eecb1d7be89cb826890f135d73c21a9638b0ba
20odeebh <31841180+20odeebh@users.noreply.github.com>
2021-01-27 23:54:05 +0900

    Update 콜라츠 추측.cpp

--- eunbin/콜라츠 추측.cpp
+++ eunbin/콜라츠 추측.cpp
@@ -4,17 +4,13 @@
 using namespace std;
 
 int solution(int num) {
+    long long n=num;
     int answer = 0;
-    long long n = (long)num;
-    while(answer < 500){
-        if( n ==1 ) break;
-        
-        if(n % 2 == 0) n /= 2;
-        else  n = n*3 + 1;  
-        
+    while(answer<500){
+        if(n == 1) return answer;
+        n = (n%2)? (n*3)+1: n/2;
         answer++;
-        
+                
     }
-    if(answer == 500) return -1;
-    else return answer;
+    return -1;
 }
