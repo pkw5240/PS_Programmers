16d20fa5aa3f856e1848a13bbed92eafb2dd1fc8
20odeebh <31841180+20odeebh@users.noreply.github.com>
2021-01-27 22:57:20 +0900

    Update 자릿수 더하기.cpp

--- eunbin/자릿수 더하기.cpp
+++ eunbin/자릿수 더하기.cpp
@@ -1,17 +1,12 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 int solution(int n)
 {
     int answer = 0;
-
-    string s = to_string(n);
-
-    for(int i = 0; i < s.size(); i++) answer += (s[i] - '0');
-
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    cout << "Hello Cpp" << endl;
-
+    while(n!=0){
+        answer+=n%10;
+        n/=10;
+    }
     return answer;
 }
