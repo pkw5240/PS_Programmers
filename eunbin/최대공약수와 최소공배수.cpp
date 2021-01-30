7ad7fe5a0b15f3d314f56e01f0b7e991d23c7940
20odeebh <31841180+20odeebh@users.noreply.github.com>
2021-01-30 19:14:14 +0900

    Update 최대공약수와 최소공배수.cpp

--- eunbin/최대공약수와 최소공배수.cpp
+++ eunbin/최대공약수와 최소공배수.cpp
@@ -1,25 +1,19 @@
+#include <string>
 #include <vector>
- 
+
 using namespace std;
- 
-int gcd(int a, int b) {
-    int gcd = 0;
-    int less = a < b ? a : b;
-    for (int i = 1; i < less + 1; i++) {
-        if (a%i == 0 && b%i == 0) gcd = i;       
+
+int gcd(int x,int y){
+    int result=0;
+    int less = (x<y)? x:y;
+    for(int i=1;i<=less;i++){
+        if( x%i == 0 && y%i == 0) result=i;
     }
-    return gcd;
+    return result;
 }
- 
-int lcm(int a, int b) {
-    int g = gcd(a, b);
-    if (g == 1) return a * b; 
-    return a*b/g;
-}
- 
 vector<int> solution(int n, int m) {
     vector<int> answer;
-    answer.push_back(gcd(n, m));
-    answer.push_back(lcm(n, m));
+    answer.push_back(gcd(n,m));
+    answer.push_back(n*m/gcd(n,m));
     return answer;
 }
