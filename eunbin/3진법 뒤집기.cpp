a7a3ec1841ca5f83059301ee760564a7b84602d2
20odeebh <31841180+20odeebh@users.noreply.github.com>
2021-01-30 12:57:05 +0900

    Update 3진법 뒤집기.cpp

--- eunbin/3진법 뒤집기.cpp
+++ eunbin/3진법 뒤집기.cpp
@@ -1,21 +1,15 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
+vector<int> v;
 int solution(int n) {
     int answer = 0;
-    vector<int> v;
-    
-    while(n)
-    {
-        v.push_back(n % 3);
-        n = n / 3;
+    while(n!=0){
+        v.push_back(n%3);
+        n/=3;        
     }
-    
-    reverse(v.begin(), v.end());
-    
-    for(int i = 0; i < v.size(); i++)
-        answer += pow(3, i) * v[i];
-    
+    for(int i=0;i<v.size();i++){
+        answer+=v[i]*pow(3,v.size()-i-1);
+    }
     return answer;
 }
